af5e563f2f5d6fa88e8b549eae50035131fabb41
zhou <837431137@qq.com>
2018-12-29 23:44:17 +0800

    the first blood

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	_ "2018/cn.zhou.beego/love_home/routers"
+	"fmt"
 	"github.com/astaxie/beego"
 )
 
 func main() {
+	//11100abc
+	fmt.Println("abc,hello,world")
 	beego.Run()
 }
-
